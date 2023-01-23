b86a1e8683cad4bcec337b22b5135b49502e79f9
filipenevola <filipe@quave.com.br>
2023-01-23 11:42:32 -0400

    Upgrade to Meteor 2.10

--- app/home/Home.js
+++ app/home/Home.js
@@ -36,7 +36,7 @@ export const Home = () => {
             href="https://www.quave.dev"
             className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white hover:bg-indigo-700"
           >
-            Visit quave.dev
+            Visit quave.dev :)
           </a>
         </div>
       </div>
