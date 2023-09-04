ce4d0a30c791c728f0a3a6b33033f4074ded669d
filipenevola <filipe@quave.com.br>
2023-09-04 14:45:01 -0400

    Adds rest endpoint writing to MongoDB

--- server/rest.js
+++ server/rest.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import { registerMetrics } from './metrics.js';
 import { WebApp } from 'meteor/webapp';
+import { ClicksCollection } from '../app/clicks/ClicksCollection';
 
 registerMetrics({
   path: '/api/metrics',
@@ -36,6 +37,43 @@ WebApp.expressHandlers.get('/api/load-fibonacci', (req, res) => {
   console.log(`fibonacci ${num}`);
   const fib = fibonacci(num);
   const message = `fibonacci ${num}=${fib}, ${new Date() - start}ms`;
+  console.log(message);
+
+  if (timing) {
+    res.status(200).send(JSON.stringify({ status: 'success', message }));
+    return;
+  }
+
+  res.set('Content-type', 'application/json');
+  res.status(200).send(JSON.stringify({ status: 'success' }));
+});
+
+WebApp.expressHandlers.get('/api/load-data', async (req, res) => {
+  res.set('Content-type', 'application/json');
+  const { num, timing } = req.query;
+  if (!num) {
+    res.status(400).send(
+      JSON.stringify({
+        status: 'error',
+        message: 'num query param is required',
+      })
+    );
+    return;
+  }
+  const start = new Date();
+  console.log(`data ${num}`);
+  let clicks = (await ClicksCollection.findOneAsync()) || { counts: 0 };
+
+  const loopSize = Array.from(Array(+num).keys());
+  console.log('loopSize', loopSize);
+
+  // eslint-disable-next-line no-restricted-syntax
+  for await (const i of loopSize) {
+    await ClicksCollection.upsertAsync({}, { $inc: { count: 1 } });
+    clicks = (await ClicksCollection.findOneAsync()) || { counts: 0 };
+    console.log(`iteration ${i}=${clicks.count}`);
+  }
+  const message = `data ${num}=${clicks.count}, ${new Date() - start}ms`;
   console.log(message);
 
   if (timing) {
