5313ca82e23ab149a002bc893ad228ae3cb02c8a
filipenevola <filipe@quave.com.br>
2023-09-04 14:45:01 -0400

    Adds rest endpoint writing to MongoDB

--- server/rest.js
+++ server/rest.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import { registerMetrics } from './metrics.js';
 import { WebApp } from 'meteor/webapp';
+import { ClicksCollection } from '../app/clicks/ClicksCollection';
 
 const app = express();
 
@@ -45,6 +46,43 @@ app.get('/load-fibonacci', (req, res) => {
   console.log(`fibonacci ${num}`);
   const fib = noFibers ? fibonacci(num) : Promise.await(fibonacci(num));
   const message = `fibonacci ${num}=${fib}, ${new Date() - start}ms`;
+  console.log(message);
+
+  if (timing) {
+    res.status(200).send(JSON.stringify({ status: 'success', message }));
+    return;
+  }
+
+  res.set('Content-type', 'application/json');
+  res.status(200).send(JSON.stringify({ status: 'success' }));
+});
+
+app.get('/load-data', (req, res) => {
+  res.set('Content-type', 'application/json');
+  const { num, timing } = req.query;
+  if (!num) {
+    res.status(400).send(
+      JSON.stringify({
+        status: 'error',
+        message: 'num query param is required',
+      })
+    );
+    return;
+  }
+  const start = new Date();
+  console.log(`data ${num}`);
+  let clicks = ClicksCollection.findOne() || { counts: 0 };
+
+  const loopSize = Array.from(Array(+num).keys());
+  console.log('loopSize', loopSize);
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const i of loopSize) {
+    ClicksCollection.upsert({}, { $inc: { count: 1 } });
+    clicks = ClicksCollection.findOne() || { counts: 0 };
+    console.log(`iteration ${i}=${clicks.count}`);
+  }
+  const message = `data ${num}=${clicks.count}, ${new Date() - start}ms`;
   console.log(message);
 
   if (timing) {
