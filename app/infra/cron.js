fa9bf1d1f6c03218a8cf693271a7d98ebb4dd7aa
filipenevola <filipe@quave.com.br>
2023-12-05 10:22:14 -0400

    Adds migrations example

--- app/infra/cron.js
+++ app/infra/cron.js
@@ -8,6 +8,7 @@ SyncedCron.config({
 
 Meteor.startup(() => {
   const a = 1;
+  const b = 1;
   SyncedCron.add({
     name: 'I run every 5 seconds',
     schedule: (parser) => parser.text('every 5 seconds'),
