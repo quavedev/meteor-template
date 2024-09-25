0e2c283f79271d6bd35497acfab3e3f0bfff5367
Filipe Névola <filipe@quave.com.br>
2024-09-25 02:47:02 -0400

    Uses React Compiler

--- client/main.js
+++ client/main.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { createRoot } from 'react-dom/client';
 import { App } from '../app/general/App';
 
 Meteor.startup(() => {
   const root = createRoot(document.getElementById('app'));
-  root.render(<App />);
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
 });
