23355b9fa0c353dd9ca67ae08d101f64886698fe
Filipe Névola <filipe@quave.com.br>
2024-10-07 12:12:59 -0300

    Adds a more complete example of job

--- app/infra/cron.js
+++ app/infra/cron.js
@@ -6,13 +6,34 @@ SyncedCron.config({
   log: true,
 });
 
+function sleepSeconds(number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, number * 1000);
+  });
+}
+
 Meteor.startup(() => {
   SyncedCron.add({
-    name: 'I run every 5 seconds',
-    schedule: (parser) => parser.text('every 5 seconds'),
-    job: () => {
+    name: 'I run every 1 minute for 10 seconds',
+    schedule: (parser) => parser.text('every 1 minute'),
+    job: async () => {
       // eslint-disable-next-line no-console
-      console.log('cron job running');
+      console.log(`I'll start now ${new Date()}`);
+      await sleepSeconds(10);
+      // eslint-disable-next-line no-console
+      console.log(`I've finished now ${new Date()}`);
+    },
+    onSuccess: async ({ intendedAt }) => {
+      // eslint-disable-next-line no-console
+      console.log(
+        `cron job finished after persist in the database now ${new Date()}, I took ${new Date() - intendedAt}ms`
+      );
+    },
+    onError: async ({ error, intendedAt }) => {
+      console.error(
+        `cron job errored after persist in the database now ${new Date()}, I took ${new Date() - intendedAt}ms`,
+        error
+      );
     },
   });
   SyncedCron.start();
